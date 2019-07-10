397b4a65c2dd4b8a57e1eb344b6c0ef2efdb181c
fradleybrancis <fradleybrancis@gmail.com>
2019-07-09 19:00:12 -0700

    add react fragments, remove console logs

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import AllProducts from './AllProducts.jsx';
 import json from '../../../data.json';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: [],
+    };
   }
 
   componentDidMount() {
-    console.log(json.groups)
+    this.setState({ data: json.groups });
   }
 
   render() {
+    const { data } = this.state;
     return (
       <React.Fragment>
-        <div> Hello from React Yeaaa boy </div>
+        <h1> Williams-Sonoma </h1>
+        <div className="allProducts">
+          <AllProducts className="allProducts" data={data}/>
+        </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
