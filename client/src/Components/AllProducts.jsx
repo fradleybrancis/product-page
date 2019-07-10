6d5c3a8719ac4384866e6cd7ff2d0712b7fc6366
fradleybrancis <fradleybrancis@gmail.com>
2019-07-09 22:36:37 -0700

    add key prop to each Product component

--- client/src/Components/AllProducts.jsx
+++ client/src/Components/AllProducts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Product from './Product.jsx';
 
-const AllProducts = props => props.data.map(currentProduct => (
-  <Product className="product" product={currentProduct} />
-));
+const AllProducts = props => props.data.map((currentProduct, index) => {
+  return <Product className="product" product={currentProduct} key={index} />;
+});
 export default AllProducts;
