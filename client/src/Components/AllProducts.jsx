faed11afc9664486ddef20d6316c38b0945e234d
fradleybrancis <fradleybrancis@gmail.com>
2019-07-09 17:21:25 -0700

    clean up code

--- client/src/Components/AllProducts.jsx
+++ client/src/Components/AllProducts.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import Product from './Product.jsx';
 
-const AllProducts = props =>
-  props.data.map(currentProduct => (
-    <Product className="product" product={currentProduct}/>
-    // <div className="product">
-    //   <div>{ product.name }</div>
-    //   <div className="productImage">
-    //     <img src={product.hero.href} alt={product.hero.alt} />
-    //     <div className="price">${product.priceRange.selling.high}</div>
-    //   </div>
-    // </div>
-  ));
+const AllProducts = props => props.data.map(currentProduct => (
+  <Product className="product" product={currentProduct} />
+));
 export default AllProducts;
